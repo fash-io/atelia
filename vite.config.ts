f14f1b8c4dad197f14c280827f08fdf1c24089e4
fash-io <favourfasi46@gmail.com>
2026-07-29 19:28:25 +0100

    next

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { nitro } from 'nitro/vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
@@ -8,6 +9,9 @@ export default defineConfig({
     plugins: [
         tsConfigPaths(),
         tailwindcss(),
+        nitro({
+            preset: 'vercel',
+        }),
         tanstackStart(),
         viteReact(),
     ],
